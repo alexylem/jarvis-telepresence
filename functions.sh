4d7ed272c1e5ed754c433d589ddb22a9fe1a1f00
Alexandre Mély <alexandre.mely@gmail.com>
2017-04-20 16:03:30 +0200

    dump cmd in troubleshooting mode

--- functions.sh
+++ functions.sh
@@ -10,5 +10,7 @@ pg_uv4l_start () {
     jv_debug "Starting UV4L server on http://$jv_ip:$pg_uv4l_port/stream/webrtc"
     $pv_uv4l_password && local password="--server-option '--user-password=$pv_uv4l_password'" || local password=""
     $verbose && local verbosity="-v7" || local verbosity="-v3"
-    uv4l --auto-video_nr --driver raspicam --encoding h264 --width $pg_uv4l_with --height $pg_uv4l_height --server-option "--port=$pg_uv4l_port" $password $verbosity
+    local cmd="uv4l --auto-video_nr --driver raspicam --encoding h264 --width $pg_uv4l_with --height $pg_uv4l_height --server-option '--port=$pg_uv4l_port' $verbosity"
+    $verbose && jv_debug "$cmd"
+    eval $cmd $password
 }
